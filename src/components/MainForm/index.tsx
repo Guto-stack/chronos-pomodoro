75922b34c68a6e8146a169d19a27ab226eee122f
Augusto Cezar <augustoccfaria@gmail.com>
2026-01-21 10:09:58 -0300

    deixando de utilisar useState e passando tuydo para useReducer

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -7,12 +7,12 @@ import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
-import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
+import { TaskActionTypes } from "../../contexts/TaskContext/taskActions";
 
 
 export function MainForm(){
 
-  const { setState, state } = useTaskContext();
+  const { dispatch, state } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   const nextCycle = getNextCycle(state.currentCycle);
@@ -40,37 +40,11 @@ export function MainForm(){
     type: nextCycleType,
    };
 
-   const secondsRemaining = newTask.duration * 60
-
-  setState(prevState => {
-    return {
-      ...prevState,
-      config: {...prevState.config},
-      activeTask: newTask,
-      currentCycle: nextCycle,
-      secondsRemaining, //conferir
-      formatedSecondsRemaining: formatSecondsToMinutes(secondsRemaining), //conferir
-      tasks: [...prevState.tasks, newTask]
-      };
-    });
-
+   dispatch({type: TaskActionTypes.START_TASK, payload: newTask});
   }
 
   function handleInterruptTask(){
-     setState(prevState => {
-    return {
-        ...prevState,
-        activeTask: null,
-        secondsRemaining: 0,
-        formatedSecondsRemaining: '00:00',
-        tasks: prevState.tasks.map(task => {
-          if(prevState.activeTask.id && prevState.activeTask.id === task.id) {
-          return {...task, interruptDate: Date.now() };
-          }
-          return task;
-        }),
-      };
-    });
+    dispatch({ type: TaskActionTypes.INTERRUPT_TASK})
   }
 
     return(
--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -4,3 +4,30 @@
 // action <- ação disparada, geralmente é um objeyo com o type e opcionalmente payload
 // type <- o tipo da ação, geralmente uma string
 //payload <- os dados extras enviados juntos com a action, se necessario para atualizar o  estado
+
+
+import type { TaskModel } from "../../models/TaskModel"
+
+export enum TaskActionTypes {
+    START_TASK = 'START_TASK',
+    INTERRUPT_TASK = 'INTERRUPT_TASK',
+    RESET_STATE = 'RESET_STATE',
+}
+
+    export type TaskActionsWithPayLoad = 
+    {
+        type: TaskActionTypes.START_TASK; 
+        payload: TaskModel;
+    };
+
+    export type TaskActionWithoutPayLoad =
+|   {
+        type: TaskActionTypes.INTERRUPT_TASK; 
+    }
+|   {
+        type: TaskActionTypes.RESET_STATE; 
+    };
+
+    export type TaskActionModel =
+    | TaskActionsWithPayLoad
+    | TaskActionWithoutPayLoad;
